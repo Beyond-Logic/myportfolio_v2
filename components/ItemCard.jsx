dc17d2f28f8f4aaaaccaae3212a768f56c76359f
Beyond-Logic <edemaero@gmail.com>
2023-04-15 13:58:38 +0100

    update ItemCard

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 const ItemCard = ({ link, image, title, children }) => {
   return (
-    <a href={link} target="_blank" rel="noreferrer">
-      <Image src={image} alt={title} width={500} height={500} />
+    <div>
+      <a href={link} target="_blank" rel="noreferrer">
+        <Image src={image} alt={title} width={500} height={500} />
+      </a>
       <br />
       <h1 className="text-base font-bold pb-4">{title}</h1>
       <div className="text-base">{children}</div>
-    </a>
+    </div>
   );
 };
 
